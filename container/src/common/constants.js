5fc8eb66bbcd0cc04f931f39784a700ed5f7464f
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-02-28 09:23:02 -0300

    Create a language to file extension

--- container/src/common/constants.js
+++ container/src/common/constants.js
@@ -8,4 +8,12 @@ const LANGUAGE = {
   GO: "GO",
   GOLANG: "GO",
 };
+
+const EXTENSIONS_PER_LANGUAGE = {
+  JS: "js",
+  PYTHON: "py",
+  GO: "go",
+};
+
+module.exports.EXTENSIONS_PER_LANGUAGE = EXTENSIONS_PER_LANGUAGE;
 module.exports.LANGUAGE = LANGUAGE;
