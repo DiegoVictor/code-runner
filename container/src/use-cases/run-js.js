dad852a0f28435c2091f5afc302bf86b653c5350
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-03-03 11:01:57 -0300

    Get template content

--- container/src/use-cases/run-js.js
+++ container/src/use-cases/run-js.js
@@ -1,18 +1,17 @@
 const { randomUUID } = require("node:crypto");
 const { writeFile } = require("node:fs/promises");
-const { getCodeByChallengeId } = require("../common/file");
-const { EXTENSIONS_PER_LANGUAGE } = require("../common/constants");
+const { getTemplateByLanguage } = require("../common/file");
+const { LANGUAGE } = require("../common/constants");
 
 const main = async ({ code, challengeId }) => {
-  const raw = await getCodeByChallengeId({
-    challengeId,
-    language: EXTENSIONS_PER_LANGUAGE.JS.toLowerCase(),
+  const raw = await getTemplateByLanguage({
+    language: LANGUAGE.JS,
   });
-  const challenge = raw.replace(/\/\/\s@code\-here/i, code);
+  const content = raw.replace(/\/\/\s@code\-here/gi, code);
 
   const runId = randomUUID();
   const filename = `/tmp/${runId}.js`;
-  await writeFile(filename, challenge);
+  await writeFile(filename, content);
 
   const { main } = require(filename);
   return main();
