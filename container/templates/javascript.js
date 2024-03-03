a04c74e4d49c4607fb090a04e3fac0664f1eb53e
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-03-03 10:58:52 -0300

    Create a simplified javascript template

---
+++ container/templates/javascript.js
@@ -0,0 +1,23 @@
+// @code-here
+
+async function executor(input) {
+  const response = {
+    input,
+  };
+
+  try {
+    const output = await run(...input);
+
+    response.output = output;
+  } catch (err) {
+    process.stdout.write(err.message);
+
+    response.output = err.message;
+  }
+
+  return response;
+}
+
+module.exports.main = async (inputs) => {
+  return Promise.all(inputs.map(executor));
+};
