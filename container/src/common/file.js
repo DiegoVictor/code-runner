003d4d35193d01edfe2df63569da04e89ca048e3
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-03-03 11:00:13 -0300

    Get content from local file

--- container/src/common/file.js
+++ container/src/common/file.js
@@ -1,18 +1,13 @@
 const { readFile } = require("node:fs/promises");
-const { getFileContent } = require("../infra/services/s3");
+const { EXTENSIONS_PER_LANGUAGE } = require("./constants");
 
-const getCodeByChallengeId = async ({ challengeId, language }) => {
-  const filePath = `/${challengeId}/${language}/challenge.${language}`;
+const getTemplateByLanguage = async ({ language }) => {
+  const filePath = `./templates/${language.toLowerCase()}.${
+    EXTENSIONS_PER_LANGUAGE[language]
+  }`;
 
-  if (process.env.LOCAL) {
-    const file = await readFile(`./samples${filePath}`);
-    return file.toString();
-  }
-
-  return getFileContent({
-    Bucket: process.env.CHALLENGE_BUCKET,
-    Key: filePath,
-  });
+  const file = await readFile(filePath);
+  return file.toString();
 };
 
-module.exports.getCodeByChallengeId = getCodeByChallengeId;
+module.exports.getTemplateByLanguage = getTemplateByLanguage;
