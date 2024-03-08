fb2e7e7e6a27d8c76090add38f953c20b36f6e61
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-03-08 01:03:59 -0300

    Add script to run container locally

---
+++ scripts/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TIMESTAMP=$(date +%s)
+CONTAINER_TAG="code-runner:$TIMESTAMP"
+
+cd container
+
+docker build --platform linux/amd64 -t $CONTAINER_TAG .
+
+PID=$(docker run --platform linux/amd64 -d -p 9000:8080 $CONTAINER_TAG)
+
+ENDPOINT="http://localhost:9000/2015-03-31/functions/function/invocations"
+
+curl $ENDPOINT -d '{"body":{"language":"js","code":"async function run(value) {\n  return value * value;\n}","inputs":[5]}}'
+# curl $ENDPOINT -d '{"body":{"language":"ts","code":"async function run(value: number) {\n  const square: number = value**2;\n  return square;\n}","inputs":[5]}}'
+# curl $ENDPOINT -d '{"body":{"language":"python","code":"def run(value):\n  return pow(value, 2)\n","inputs":[5]}}'
+# curl $ENDPOINT -d '{"body":{"language":"go","code":"func run(value interface{}) (interface{}, error) {\n  num := float64(value.(float64))\n  return num * num, nil\n}","inputs":[5]}}'
