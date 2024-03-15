4b12f218347ca741b4d5bc961586df7d03d74ae6
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-03-15 08:41:57 -0300

    Add command wrapper

---
+++ container/src/common/command.js
@@ -0,0 +1,20 @@
+const { spawn } = require("node:child_process");
+
+const command = async (cmd, args) => {
+  const cli = spawn(cmd, args);
+
+  return new Promise((resolve) => {
+    const chunks = [];
+
+    cli.stdout.on("data", (data) => {
+      chunks.push(data);
+    });
+
+    cli.stdout.on("close", () => {
+      const response = Buffer.concat(chunks).toString("utf8");
+      resolve(response);
+    });
+  });
+};
+
+module.exports.command = command;
