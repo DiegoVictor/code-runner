3f15beaa09e1144882a5f21d6fa9e19d34ffe017
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-05-13 10:53:37 -0300

    Update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,7 +11,7 @@ PID=$(docker run --platform linux/amd64 -d -p 9000:8080 $CONTAINER_TAG)
 
 ENDPOINT="http://localhost:9000/2015-03-31/functions/function/invocations"
 
-curl $ENDPOINT -d '{"body":{"language":"js","code":"async function run(value) {\n  return value * value;\n}","inputs":[5]}}'
-# curl $ENDPOINT -d '{"body":{"language":"ts","code":"async function run(value: number) {\n  const square: number = value**2;\n  return square;\n}","inputs":[5]}}'
-# curl $ENDPOINT -d '{"body":{"language":"python","code":"def run(value):\n  return pow(value, 2)\n","inputs":[5]}}'
-# curl $ENDPOINT -d '{"body":{"language":"go","code":"func run(value interface{}) (interface{}, error) {\n  num := float64(value.(float64))\n  return num * num, nil\n}","inputs":[5]}}'
+curl $ENDPOINT -d '{"language":"js","code":"async function run(value) {\n  return value * value;\n}","inputs":[5]}'
+# curl $ENDPOINT -d '{"language":"ts","code":"async function run(value: number) {\n  const square: number = value**2;\n  return square;\n}","inputs":[5]}'
+# curl $ENDPOINT -d '{"language":"python","code":"def run(value):\n  return pow(value, 2)\n","inputs":[5]}'
+# curl $ENDPOINT -d '{"language":"go","code":"func run(value interface{}) (interface{}, error) {\n  num := float64(value.(float64))\n  return num * num, nil\n}","inputs":[5]}'
