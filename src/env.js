df0bbe7e306914571b705361f7e8d7721f0c2059
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-05-16 00:02:47 -0300

    Validate required environment variables

---
+++ src/env.js
@@ -0,0 +1,23 @@
+const { z } = require("zod");
+
+const schema = z.object({
+  DATABASE_URL: z.string({
+    required_error: "Missing DATABASE_URL in environment variable",
+  }),
+  PORT: z.coerce
+    .number({
+      invalid_type_error: "PORT must be a number",
+    })
+    .default(5000),
+  NODE_ENV: z
+    .enum(["dev", "test", "prod"], {
+      invalid_type_error:
+        "NODE_ENV must one of the following: dev, test or prod",
+      required_error: "Missing NODE_ENV in environment variables",
+    })
+    .default("dev"),
+  CODERUNNER_FUNCTION: z.string().default("CodeRunnerFunction"),
+  CODERUNNER_CONTAINER_URL: z.string().url().optional(),
+});
+
+module.exports.env = schema.parse(process.env);
