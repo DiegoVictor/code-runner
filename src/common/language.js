cb75000e510fa1a3667733e341b8f50e2780cc70
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-05-18 03:18:19 -0300

    Create languages enum

---
+++ src/common/language.js
@@ -0,0 +1,12 @@
+const LANGUAGE = {
+  JS: "js",
+  JAVASCRIPT: "js",
+  TS: "ts",
+  TYPESCRIPT: "ts",
+  PYTHON: "python",
+  PY: "python",
+  GO: "go",
+  GOLANG: "go",
+};
+
+module.exports.LANGUAGE = LANGUAGE;
