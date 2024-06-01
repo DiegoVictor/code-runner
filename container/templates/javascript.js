e5d79a3a8b1dddc85d34ce6750e8794ec41ceaa8
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-06-01 12:15:13 -0300

    Capture users' code console.log's

--- container/templates/javascript.js
+++ container/templates/javascript.js
@@ -6,14 +6,24 @@ async function executor({ id, value }) {
     value,
   };
 
+  const stdout = [];
   try {
+    const write = process.stdout.write;
+    process.stdout.write = (output) => {
+      stdout.push(output.replace(/(.+)INFO\s/g, ""));
+    };
+
     const output = await run(value);
+
+    process.stdout.write = write;
 
     response.output = output;
   } catch (err) {
-    process.stdout.write(err.message);
-
     response.output = err.message;
+  }
+
+  if (stdout.length > 0) {
+    response.stdout = stdout.join("");
   }
 
   return response;
