b9d50f7310dcf90e2a18805648d6a351168e8756
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-06-09 00:15:15 -0300

    Use local prisma

--- src/migrations.js
+++ src/migrations.js
@@ -1,6 +1,6 @@
 const { execSync } = require("child_process");
 
 module.exports.handler = () => {
-  const output = execSync("npx prisma migrate deploy");
+  const output = execSync("./node_modules/.bin/prisma migrate deploy");
   console.log(output.toString());
 };
