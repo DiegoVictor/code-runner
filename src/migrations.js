64aa3ed644322f5b516d2035aaf00ff46f9a56a3
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-06-10 21:02:50 -0300

    Execute commands to check lambda user and permission

--- src/migrations.js
+++ src/migrations.js
@@ -1,6 +1,8 @@
 const { execSync } = require("child_process");
 
 module.exports.handler = () => {
+  console.log(execSync("echo $(id -u):$(id -g)").toString());
+  console.log(execSync("ls -la").toString());
   const output = execSync("./node_modules/.bin/prisma migrate deploy");
   console.log(output.toString());
 };
