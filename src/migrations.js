62c46ee212414b2f8ef3a7f861be4bd329d7385b
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-06-10 21:39:46 -0300

    Update migrations function source code

--- src/migrations.js
+++ src/migrations.js
@@ -1,8 +1,6 @@
 const { execSync } = require("child_process");
 
 module.exports.handler = () => {
-  console.log(execSync("echo $(id -u):$(id -g)").toString());
-  console.log(execSync("ls -la").toString());
   const output = execSync("./node_modules/.bin/prisma migrate deploy");
   console.log(output.toString());
 };
