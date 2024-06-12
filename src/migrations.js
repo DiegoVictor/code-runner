303d0cf9e644806c4f82e6934fcd29b90df1e664
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-06-11 22:08:55 -0300

    Update migrations handler

--- src/migrations.js
+++ src/migrations.js
@@ -1,6 +1,17 @@
-const { execSync } = require("child_process");
+const { SecretsManager } = require("@aws-sdk/client-secrets-manager");
+const { command } = require("./common/command");
 
-module.exports.handler = () => {
-  const output = execSync("./node_modules/.bin/prisma migrate deploy");
-  console.log(output.toString());
+module.exports.handler = async () => {
+  const secretsManager = new SecretsManager({});
+  const secret = await secretsManager.getSecretValue({
+    SecretId: process.env.DB_SECRET,
+  });
+  const { username, password } = JSON.parse(secret.SecretString);
+
+  const DATABASE_URL = `postgresql://${username}:${password}@${process.env.DB_HOST}/coderunner?schema=public`;
+
+  const output = await command(
+    `DATABASE_URL=${DATABASE_URL} npx prisma migrate deploy`
+  );
+  console.log(output);
 };
