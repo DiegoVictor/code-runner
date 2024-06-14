d005a97f44b29fa678f3c22ff53a4b3b1fbe0844
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-06-14 11:33:02 -0300

    Read connection URL from environment variables

--- src/migrations.js
+++ src/migrations.js
@@ -2,16 +2,16 @@ const { SecretsManager } = require("@aws-sdk/client-secrets-manager");
 const { command } = require("./common/command");
 
 module.exports.handler = async () => {
-  const secretsManager = new SecretsManager({});
-  const secret = await secretsManager.getSecretValue({
-    SecretId: process.env.DB_SECRET,
-  });
-  const { username, password } = JSON.parse(secret.SecretString);
+  // const secretsManager = new SecretsManager({});
+  // const secret = await secretsManager.getSecretValue({
+  //   SecretId: process.env.DB_SECRET,
+  // });
+  // const { username, password } = JSON.parse(secret.SecretString);
 
-  const DATABASE_URL = `postgresql://${username}:${password}@${process.env.DB_HOST}/coderunner?schema=public`;
+  // const DATABASE_URL = `postgresql://${username}:${password}@${process.env.DB_HOST}/coderunner?schema=public`;
 
   const output = await command(
-    `DATABASE_URL=${DATABASE_URL} npx prisma migrate deploy`
+    `DATABASE_URL=${process.env.DATABASE_URL} npx prisma migrate deploy`
   );
   console.log(output);
 };
