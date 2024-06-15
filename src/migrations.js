cb5d0658bffa57cd531448c10f097660a549882e
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-06-15 12:02:39 -0300

    Use spawnSync to set DATABASE_URL environment variable

--- src/migrations.js
+++ src/migrations.js
@@ -1,6 +1,5 @@
 const { SecretsManager } = require("@aws-sdk/client-secrets-manager");
-// const { command } = require("./common/command");
-const { execSync } = require("child_process");
+const { spawnSync } = require("child_process");
 
 module.exports.handler = async () => {
   // const secretsManager = new SecretsManager({});
@@ -11,6 +10,20 @@ module.exports.handler = async () => {
 
   // const DATABASE_URL = `postgresql://${username}:${password}@${process.env.DB_HOST}/coderunner?schema=public`;
 
-  const output = execSync("./node_modules/.bin/prisma migrate deploy");
+
+  const { output, error } = spawnSync(
+    "./node_modules/.bin/prisma migrate deploy",
+    {
+      shell: true,
+      env: {
+        ...process.env,
+        DATABASE_URL: `postgresql://app_user:${process.env.DATABASE_PASSWORD}@${process.env.DB_HOST}/coderunner`,
+      },
+    }
+  );
+
+  if (error) {
+    console.log(error.message, error.stack);
+  }
   console.log(output.toString());
 };
