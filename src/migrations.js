2f809efba2ea88f2c81669fc4274eb0f8890ee50
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-07-08 13:04:25 -0300

    Get URL directly

--- src/migrations.js
+++ src/migrations.js
@@ -1,8 +1,12 @@
 const { execSync } = require("child_process");
-const { manager } = require("./infra/database/manager");
+const { secretManager } = require("./infra/services/secret-manager");
 
 module.exports.handler = async () => {
-  const DATABASE_URL = await manager.getURL();
+  const { username, password } = await secretManager.getSecret(
+    process.env.CLUSTER_SECRET_ID
+  );
+  const DATABASE_URL = `postgresql://${username}:${password}@${process.env.CLUSTER_URL}/coderunner?schema=public`;
+
   const output = execSync(
     `DATABASE_URL=${DATABASE_URL} ./node_modules/.bin/prisma migrate deploy`
   );
