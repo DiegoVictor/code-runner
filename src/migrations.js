7bdaf6b4542c72af35acfdcb50a1e8b20cb2f560
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-07-08 11:18:44 -0300

    Use database manager to get connection URL

--- src/migrations.js
+++ src/migrations.js
@@ -1,17 +1,10 @@
-const { SecretsManager } = require("@aws-sdk/client-secrets-manager");
 const { execSync } = require("child_process");
-const { getURL } = require("./common/database");
+const { manager } = require("./infra/database/manager");
 
 module.exports.handler = async () => {
-  const secretsManager = new SecretsManager({});
-  const secret = await secretsManager.getSecretValue({
-    SecretId: process.env.CLUSTER_SECRET_ID,
-  });
-  const { username, password } = JSON.parse(secret.SecretString);
-
-  const DATABASE_URL = getURL(username, password);
+  const DATABASE_URL = await manager.getURL(username, password);
   const output = execSync(
-    `DATABASE_URL="${DATABASE_URL}" ./node_modules/.bin/prisma migrate deploy`
+    `DATABASE_URL=${DATABASE_URL} ./node_modules/.bin/prisma migrate deploy`
   );
 
   console.log(output.toString());
