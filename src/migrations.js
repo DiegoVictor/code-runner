8c3845e76df13f421343648f88fa25844f983040
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-07-08 14:42:58 -0300

    Wrap database url with quotes

--- src/migrations.js
+++ src/migrations.js
@@ -8,7 +8,7 @@ module.exports.handler = async () => {
   const DATABASE_URL = `postgresql://${username}:${password}@${process.env.CLUSTER_URL}/coderunner?schema=public`;
 
   const output = execSync(
-    `DATABASE_URL=${DATABASE_URL} ./node_modules/.bin/prisma migrate deploy`
+    `DATABASE_URL="${DATABASE_URL}" ./node_modules/.bin/prisma migrate deploy`
   );
 
   console.log(output.toString());
