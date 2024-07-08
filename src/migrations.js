a14f4f7d086ad6e3f99cf46e1256468802fc4c0e
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-07-08 12:03:12 -0300

    Stop passing unnecessary parameters

--- src/migrations.js
+++ src/migrations.js
@@ -2,7 +2,7 @@ const { execSync } = require("child_process");
 const { manager } = require("./infra/database/manager");
 
 module.exports.handler = async () => {
-  const DATABASE_URL = await manager.getURL(username, password);
+  const DATABASE_URL = await manager.getURL();
   const output = execSync(
     `DATABASE_URL=${DATABASE_URL} ./node_modules/.bin/prisma migrate deploy`
   );
