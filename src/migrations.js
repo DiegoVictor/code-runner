1a6fee8cdc4222b46b8f4c447a6949d020b7d8ea
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-07-10 18:41:03 -0300

    Encode password before use it

--- src/migrations.js
+++ src/migrations.js
@@ -5,7 +5,8 @@ module.exports.handler = async () => {
   const { username, password } = await secretManager.getSecret(
     process.env.CLUSTER_SECRET_ID
   );
-  const DATABASE_URL = `postgresql://${username}:${password}@${process.env.CLUSTER_URL}/coderunner?schema=public`;
+  const pass = encodeURIComponent(password);
+  const DATABASE_URL = `postgresql://${username}:${pass}@${process.env.CLUSTER_URL}/coderunner?schema=public`;
 
   const output = execSync(
     `DATABASE_URL="${DATABASE_URL}" ./node_modules/.bin/prisma migrate deploy`
