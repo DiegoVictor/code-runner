b01d75fb9e146d2af3348fe1e75672db848c3159
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-07-30 11:34:37 -0300

    Update script

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,7 +11,7 @@ PID=$(docker run --platform linux/amd64 -d -p 9000:8080 $CONTAINER_TAG)
 
 ENDPOINT="http://localhost:9000/2015-03-31/functions/function/invocations"
 
-curl $ENDPOINT -d '{"language":"js","code":"async function run(value) {\n  return value * value;\n}","inputs":[{"id":"clwihx1hx000008l83dl877wj","value":5}]}'
-# curl $ENDPOINT -d '{"language":"ts","code":"async function run(value: number) {\n  const square: number = value**2;\n  return square;\n}","inputs":[{"id":"clwihx1hx000008l83dl877wj","value":5}]}'
-# curl $ENDPOINT -d '{"language":"python","code":"def run(value):\n  return pow(value, 2)\n","inputs":[{"id":"clwihx1hx000008l83dl877wj","value":5}]}'
-# curl $ENDPOINT -d '{"language":"go","code":"func run(value interface{}) (interface{}, error) {\n  num := float64(value.(float64))\n  return num * num, nil\n}","inputs":[{"id":"clwihx1hx000008l83dl877wj","value":5}]}'
+curl $ENDPOINT -d '{"language":"js","code":"async function run({ input }) {\n  return input * input;\n}","inputs":[{"id":"clwihx1hx000008l83dl877wj","value":{"input":5}}]}'
+# curl $ENDPOINT -d '{"language":"ts","code":"async function run({ input }: Record<string, number>) {\n  const square: number = input**2;\n  return square;\n}","inputs":[{"id":"clwihx1hx000008l83dl877wj","value":{"input":5}}]}'
+# curl $ENDPOINT -d '{"language":"python","code":"def run(params):\n  return pow(params['\''input'\''], 2)\n","inputs":[{"id":"clwihx1hx000008l83dl877wj","value":{"input":5}}]}'
+# curl $ENDPOINT -d '{"language":"go","code":"func run(params map[string]interface{}) (interface{}, error) {\n  num := float64(params[\"input\"].(float64))\n  return num * num, nil\n}","inputs":[{"id":"clwihx1hx000008l83dl877wj","value":{"input":5}}]}'
