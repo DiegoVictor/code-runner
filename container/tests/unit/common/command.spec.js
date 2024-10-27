e754f3ef55d80ade7dcae7a6f3dba337e0e0fc73
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-10-27 13:56:01 -0300

    Test stderr capturing

--- container/tests/unit/common/command.spec.js
+++ container/tests/unit/common/command.spec.js
@@ -37,4 +37,40 @@ describe("Command", () => {
     expect(mockSpawn).toHaveBeenCalledWith(cmd, args);
     expect(response).toBe(JSON.stringify(data));
   });
+
+  it("should be able to capture stderr", async () => {
+    const data = { error: "unexpected error" };
+    mockSpawn.mockReturnValueOnce({
+      stderr: {
+        on: (eventName, func) => {
+          switch (eventName) {
+            case "data":
+              func(Buffer.from(JSON.stringify(data)));
+              break;
+          }
+        },
+      },
+      stdout: {
+        on: (eventName, func) => {
+          switch (eventName) {
+            case "close":
+              func();
+              break;
+          }
+        },
+      },
+    });
+
+    const log = jest.spyOn(console, "log");
+    log.mockImplementationOnce(() => {});
+
+    const cmd = "echo";
+    const args = ["Hellow World"];
+
+    const response = await command(cmd, args);
+
+    expect(mockSpawn).toHaveBeenCalledWith(cmd, args);
+    expect(response).toBe("");
+    expect(log).toHaveBeenCalledWith(JSON.stringify(data));
+  });
 });
